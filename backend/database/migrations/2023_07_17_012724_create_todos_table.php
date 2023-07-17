87aaaa61baadadcba3cff422d9bcce59085fa7eb
A.kido <a.kido@ekidonoMacBook-Pro.local>
2023-07-17 11:09:22 +0900

    feat: todosテーブルを追加

---
+++ backend/database/migrations/2023_07_17_012724_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');  // 参照先が削除されたら同時に削除
+            $table->string('todos');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
